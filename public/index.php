8d428f570f96d0ecf4f5ee4eaf771ee03bfa22dd
buonzz <buonzz@gmail.com>
2015-06-13 15:41:42 +0000

    cleanup

---
+++ public/index.php
@@ -0,0 +1,21 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
+header('Access-Control-Allow-Credentials: true');
+
+require_once '../vendor/restler.php';
+require_once "../app/config/app.php";
+
+use Luracast\Restler\Restler;
+
+$r = new Restler();
+
+foreach($config['loaded_controllers'] as $c)
+	$r->addAPIClass($c);
+
+$r->addAPIClass($config['default_controller'], '');
+
+$r->addAPIClass('Resources');
+$r->handle();
