c2d833e2d8978c1c28f40642f89a7d5ceec30e59
buonzz <buonzz@gmail.com>
2015-06-13 15:46:45 +0000

    updated composer

---
+++ app/config/app.php
@@ -0,0 +1,4 @@
+<?php
+
+$config['loaded_controllers'] = array();
+$config['default_controller'] = 'Welcome';
---
+++ app/controllers/Welcome.php
@@ -0,0 +1,7 @@
+<?php
+
+class Welcome{
+	public function get(){		
+		return array("Your API" => "v1.0");
+	}
+}
