8665f725f59d808495bc3505c48de6f973eca785
buonzz <buonzz@gmail.com>
2015-06-14 15:44:45 +0000

    cleanup dev

--- app/controllers/Pageview.php
+++ app/controllers/Pageview.php
@@ -1,65 +1,31 @@
 <?php
 
 class Pageview{
-	private $cache;
+
+	private $repository;
 
 	public function __construct(){
-		
-		if(class_exists('Memcached'))
-			$this->cache = new Memcached();
-		else	
-			$this->cache = new Memcache();
-		
-		$this->cache->addServer('localhost', 11211);
 
+		$this->repository = new PageviewRepository(); 
+	
 	}
 
-	public function get(){
-		
-		/*  raw hit counter */
-		$v =  $this->cache->get('hits');
-		
-		if($v === FALSE)
-			$this->cache->set('hits', 1);		
-		else
-			$this->cache->increment('hits', 1);
+	public function get(){		
+	
+		$this->repository->setOrIncrement('hits');
 
-		/*  by ip  */
-		$v =  $this->cache->get($_SERVER['REMOTE_ADDR']);
-		
-		if($v === FALSE)
-			$this->cache->set($_SERVER['REMOTE_ADDR'], 1);		
-		else
-			$this->cache->increment($_SERVER['REMOTE_ADDR'], 1);
+		$this->repository->setOrIncrement($_SERVER['REMOTE_ADDR']);
 
-
-		/* store unique ips */
-		$v =  $this->cache->get('unique_ips');
-		if($v === FALSE)
-		{
-			$uniques = array($_SERVER['REMOTE_ADDR']);
-			$this->cache->set('unique_ips', $uniques);		
-		}
-		elseif(!is_array($v)){
-			$uniques = array($_SERVER['REMOTE_ADDR']);
-			$this->cache->set('unique_ips', $uniques);	
-		}
-		else
-		{
-			if(!in_array($_SERVER['REMOTE_ADDR'], $v))
-			{
-				$v[] = $_SERVER['REMOTE_ADDR'];
-				$this->cache->set($v, $v);
-			}
-		}
-
+		$this->repository->addToList('unique_ips', $_SERVER['REMOTE_ADDR']);
 
 		return array("hits" => "success");
 	}
 
 	public function getHits(){
-		$hits = $this->cache->get('hits');
-		$unique_ips = $this->cache->get('unique_ips');
+
+		$hits = $this->repository->get('hits');
+		
+		$unique_ips = $this->repository->get('unique_ips');
 
 		return array("hits" => $hits, 'unique_ips' => $unique_ips);
 	}
---
+++ app/repositories/PageviewRepository.php
@@ -0,0 +1,55 @@
+<?php
+
+class PageviewRepository{
+	
+	private $cache;
+
+	public function __construct(){
+		
+		if(class_exists('Memcached'))
+			$this->cache = new Memcached();
+		else	
+			$this->cache = new Memcache();
+		
+		$this->cache->addServer('localhost', 11211);
+
+	}
+
+	public function get($key){
+		return $this->cache->get($key);
+	}
+
+	public function setOrIncrement($key){
+
+		$value =  $this->cache->get($key);
+		
+		if($value === FALSE)
+			$this->cache->set($key, 1);		
+		else
+			$this->cache->increment($key, 1);
+	}
+
+	public function addToList($listname, $value){
+
+		$v =  $this->cache->get($listname);
+		
+		if($v === FALSE)
+		{
+			$mylist = array($value);
+			$this->cache->set($listname, $mylist);		
+		}
+		elseif(!is_array($v)){
+			$mylist = array($value);
+			$this->cache->set($listname, $mylist);	
+		}
+		else
+		{
+			if(!in_array($value, $v))
+			{
+				$v[] = $value;
+				$this->cache->set($listname, $v);
+			}
+		}	
+	}
+	
+}
