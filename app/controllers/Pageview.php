9411c88ed8e2239958e14950d2f82ceabc8a46d4
buonzz <buonzz@gmail.com>
2015-06-14 17:26:05 +0000

    added hosts, referers and  user agents items

--- app/controllers/Pageview.php
+++ app/controllers/Pageview.php
@@ -16,17 +16,35 @@ class Pageview{
 
 		$this->repository->setOrIncrement($_SERVER['REMOTE_ADDR']);
 
-		$this->repository->addToList('unique_ips', $_SERVER['REMOTE_ADDR']);
+		$this->repository->addToList('ips', $_SERVER['REMOTE_ADDR']);
 
+		$this->repository->addToList('user_agents', $_SERVER['HTTP_USER_AGENT']);
+
+		if(strlen($_SERVER['HTTP_REFERER'])>0)
+		{
+			$this->repository->addToList('referers', $_SERVER['HTTP_REFERER']);
+			
+			$host = parse_url($_SERVER['HTTP_REFERER'], PHP_URL_HOST);
+
+			$this->repository->addToList('hosts', $host);
+		}
+		
 		return array("hits" => "success");
 	}
 
 	public function getHits(){
 
 		$hits = $this->repository->get('hits');
-		
-		$unique_ips = $this->repository->get('unique_ips');
+		$ips = $this->repository->get('ips');
+		$referers = $this->repository->get('referers');
+		$user_agents = $this->repository->get('user_agents');
+		$hosts = $this->repository->get('hosts');
 
-		return array("hits" => $hits, 'unique_ips' => $unique_ips);
+		return array("hits" => $hits, 
+				'ips' => $ips,
+				'referers' => $referers,
+				'user_agents' => $user_agents,
+				'hosts' => $hosts
+				);
 	}
 }
