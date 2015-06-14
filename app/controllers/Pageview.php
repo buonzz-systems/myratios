1c20f5f6ce3c302c1426920a2b3c56404bd42be4
buonzz <buonzz@gmail.com>
2015-06-14 11:41:38 +0800

    added unique ips

--- app/controllers/Pageview.php
+++ app/controllers/Pageview.php
@@ -11,6 +11,8 @@ class Pageview{
 	}
 
 	public function get(){
+		
+		/*  raw hit counter */
 		$v =  $this->cache->get('hits');
 		
 		if($v === FALSE)
@@ -18,11 +20,39 @@ class Pageview{
 		else
 			$this->cache->increment('hits', 1);
 
+		/*  by ip  */
+		$v =  $this->cache->get($_SERVER['REMOTE_ADDR']);
+		
+		if($v === FALSE)
+			$this->cache->set($_SERVER['REMOTE_ADDR'], 1);		
+		else
+			$this->cache->increment($_SERVER['REMOTE_ADDR'], 1);
+
+
+		/* store unique ips */
+		$v =  $this->cache->get('unique_ips');
+		if($v === FALSE)
+		{
+			$uniques = array($_SERVER['REMOTE_ADDR']);
+			$this->cache->set('unique_ips', $uniques);		
+		}
+		else
+		{
+			if(!in_array($_SERVER['REMOTE_ADDR'], $v))
+			{
+				$v[] = $_SERVER['REMOTE_ADDR'];
+				$this->cache->set($v, $uniques);
+			}
+		}
+
+
 		return array("hits" => "success");
 	}
 
 	public function getHits(){
-		$hits = $this->cache->get('hits');		
-		return array("hits" => $hits);
+		$hits = $this->cache->get('hits');
+		$unique_ips = $this->cache->get('unique_ips');
+
+		return array("hits" => $hits, 'unique_ips' => $unique_ips);
 	}
 }
