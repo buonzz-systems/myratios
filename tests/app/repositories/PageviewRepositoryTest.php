f2c09279f21edc2b4dd4842cb6e7ad320bec8409
buonzz <buonzz@gmail.com>
2015-06-14 16:07:19 +0000

    fixed tests

--- tests/app/repositories/PageviewRepositoryTest.php
+++ tests/app/repositories/PageviewRepositoryTest.php
@@ -20,4 +20,35 @@ class PageviewRepositoryTest extends PHPUnit_Framework_TestCase{
   		
   }
 
+  public function testSetOrIncrement(){
+  
+  	$repo = new PageviewRepository();
+  	
+  	$key = 'test'. rand(100,10000);
+  	
+  	$repo->setOrIncrement($key);
+
+  	$v = $repo->get($key);
+
+  	$this->assertTrue($v == 1);
+  
+  }
+
+   public function testAddToList(){
+  
+  	$repo = new PageviewRepository();
+  	
+  	$key = 'list'. rand(100,10000);
+  	
+  	$repo->addToList($key, 'item1');
+  	$repo->addToList($key, 'item2');
+
+  	$v = $repo->get($key);
+
+  	$this->assertTrue($v[0] == 'item1');
+  	$this->assertTrue($v[1] == 'item2');
+  
+  }
+
+
 }
