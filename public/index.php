7bdea5369e5113b5ed9b89d756e3e375ef5d2f4d
buonzz <buonzz@gmail.com>
2015-06-20 15:27:08 +0000

    used custom fork

--- public/index.php
+++ public/index.php
@@ -5,12 +5,11 @@ header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 header('Access-Control-Allow-Credentials: true');
 
-require_once '../vendor/restler.php';
 require_once "../app/config/app.php";
 
-use Luracast\Restler\Restler;
+use \Luracast\Restler\Restler;
 
-$r = new Restler();
+$r = new \Luracast\Restler\Restler();
 
 foreach($config['loaded_controllers'] as $c)
 	$r->addAPIClass($c);
