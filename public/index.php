297501a664f04ba28ec6e1618c15396e4b6595d5
buonzz <buonzz@gmail.com>
2015-06-20 15:39:41 +0000

    fixed autoloading stuffs

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ header('Access-Control-Allow-Methods: GET, POST, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, Accept, Authorization, X-Request-With');
 header('Access-Control-Allow-Credentials: true');
 
+require_once "../vendor/autoload.php";
 require_once "../app/config/app.php";
 
 use \Luracast\Restler\Restler;
