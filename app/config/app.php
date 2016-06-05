cb463ee9054e73e5ed39e885cc9180833f47a965
buonzz <buonzz@gmail.com>
2016-06-05 21:18:45 +0800

    switched to logstash

--- app/config/app.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-$config['loaded_controllers'] = array('Pageviews', 'Accounts', 'Ads',
-								'Affiliates', 'Campaigns','Clicks', 'Impressions', 
-								'Networks', 'Sites','Thumbnails','Tours');
-$config['default_controller'] = 'Welcome';
